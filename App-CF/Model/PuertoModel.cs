37d7e8b2a85bd12c23d65fc683ea568f05968582
MauLang18 <maulangbonilla.18@gmail.com>
2024-07-30 14:00:33 -0600

    feat(origen): add dynamics whs

---
+++ App-CF/Model/PuertoModel.cs
@@ -0,0 +1,11 @@
+﻿namespace App_CF.Model
+{
+    public class PuertoModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Whs { get; set; }
+        public int Estado { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
